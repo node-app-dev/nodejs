370259d61244d8f6c1bfb83ba683277852e2e24b
Jabed Hossain <73439499+node-app-dev@users.noreply.github.com>
2021-07-19 19:22:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on ${'\033[35m'}http://127.0.0.1:${port}${'\033[0m'}`);
 });
